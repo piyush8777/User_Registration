0476657408c14821354f5a5a12768dcf944797d9
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-19 04:06:47 +0530

    Added :format of mobile number

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration Portal");
-		Registration.checkLastName();
+		Registration.checkPhoneNumber();
 	}
-
 }
--- Registration.java
+++ Registration.java
@@ -16,15 +16,14 @@ import java.util.regex.Pattern;
 public class Registration {
 static Scanner userInput = new Scanner(System.in);
 	
-	//*To check the Format of EmailID*
-	public static void checkEmailID() { // UC3
-		System.out.println("Enter the valid EmailId: ");
-		String check = userInput.next();
-		String emailId = "^[a-zA-Z0-9]{1,}[.a-zA-Z0-9]*@[a-zA-Z0-9]{1,}.[a-z]{1,3}(.[a-z]{2})*$";
-		if (Pattern.matches(emailId, check))
-			System.out.println("Correct Format of email: ");
-		else
-			System.out.println("Invalid email  : please try again!!!");
-	}
-
+	//* To check the Format of Mobile number *
+		public static void checkPhoneNumber() { 
+			String phoneNumber = "([0-9]{2})[ ]([0-9]{10})$";
+			System.out.println("Enter the PhoneNo: ");
+			String check = userInput.nextLine();
+			if (Pattern.matches(phoneNumber, check))
+				System.out.println("Correct Format of Phone no");
+			else
+				System.out.println("Invalid: please try again!!!");
+		}
 }
