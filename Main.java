7e7de950671fe13458b6528f977646f45176a0e8
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-19 04:10:09 +0530

    Added :Maximim 8 digit password

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.bridgelabz;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration Portal");
-		Registration.checkPhoneNumber();
+		Registration.checkPassword();
 	}
 }
