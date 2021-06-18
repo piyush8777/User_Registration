d5907a1fe68670cf841346f53e059c2a28ec5ec3
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-19 04:13:29 +0530

    Added :Password should have one uppercase

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration Portal");
-		Registration.checkPassword();
+		Registration.checkPatternPassword();
+		
 	}
 }
