c84eeac4c928f29d3cc9fd20547f4c01faae4924
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-21 11:34:34 +0530

    Added :a special character

--- Registration.java
+++ Registration.java
@@ -16,14 +16,15 @@ import java.util.regex.Pattern;
 public class Registration {
 	static Scanner userInput = new Scanner(System.in);
 
-	// * To Validate the Password :it should contain atleast 1 Numeric digit*
-	public static void checkPattern1Password() { 
-		String password =	"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}";
-		System.out.println("Enter the 8 digits Password should contain atleast 1 upperCase and numeric digit : ");
+	// * To Validate the Password :it should contain atleast1 UpperCase 1 Numeric
+	// digit,1 special character*
+	public static void checkPattern2Password() {
+		String password = "^(?=.*\\d)([a-z])*(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
+		System.out.println("Enter the minimum 8 digits Password should contain atleast 1 upperCase and numeric digit and 1 Special character : ");
 		String check = userInput.next();
 		if (Pattern.matches(password, check))
 			System.out.println("Valid password");
 		else
-			System.out.println("Invalid: please try again!!!");
+			System.out.println("Error !!! please check your Format");
 	}
 }
