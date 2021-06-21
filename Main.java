1030558996b22d223475311c382575bf1eea5ee4
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-21 11:15:17 +0530

    Added : To add email

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to user registration Portal");
-		Registration.checkLastName();
-	}
+			System.out.println("Welcome to user registration Portal");
+			Registration.checkEmailID();
+		}
 
 }
