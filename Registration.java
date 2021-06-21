d24949f3e1d089faa446a6522b4dd53849b5f3cd
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-21 10:44:08 +0530

    Added :Password should have one uppercase

--- Registration.java
+++ Registration.java
@@ -14,16 +14,16 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Registration {
-static Scanner userInput = new Scanner(System.in);
-	
-	//* To check the Format of Mobile number *
-		public static void checkPhoneNumber() { 
-			String phoneNumber = "([0-9]{2})[ ]([0-9]{10})$";
-			System.out.println("Enter the PhoneNo: ");
-			String check = userInput.nextLine();
-			if (Pattern.matches(phoneNumber, check))
-				System.out.println("Correct Format of Phone no");
-			else
-				System.out.println("Invalid: please try again!!!");
-		}
+	static Scanner userInput = new Scanner(System.in);
+
+	// * To Validate the Password :it should contain atleast 1 upperCase*
+	public static void checkPatternPassword() { 
+		String password = "^(\\d[a-z])*(?=.*[A-Z]).{8,}$";
+		System.out.println("Enter the 8 digits Password should contain atleast 1 UpperCase : ");
+		String check = userInput.next();
+		if (Pattern.matches(password, check))
+			System.out.println("Valid Password : you may proceed further");
+		else
+			System.out.println("Invalid: please try again!!!");
+	}
 }
